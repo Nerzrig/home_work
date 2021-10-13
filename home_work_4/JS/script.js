103902fb2843d5574a13ca72a0e5c221a8a77c8b
Georgiy Blagoobrazov <40820747+Nerzrig@users.noreply.github.com>
2021-10-13 08:19:47 +0300

    Add files via upload

--- home_work_4/JS/script.js
+++ home_work_4/JS/script.js
@@ -3,24 +3,22 @@ let firstNumber = +prompt ( 'Enter the first number' );
 let secondNumber = +prompt ( 'Enter the second number' );
 	
 switch (mathOperation){
-	case "add", "+":
-		alert( firstNumber + secondNumber );
+	case "add":
+		alert( `${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber}` );
 		break;
-	case "sub", "-":
-		alert( firstNumber - secondNumber );
+	case "sub":
+		alert( `${firstNumber} - ${secondNumber} = ${firstNumber - secondNumber}` );
 		break;
-	case "mult", "*":
-		alert( firstNumber * secondNumber );
+	case "mult":
+		alert( `${firstNumber} * ${secondNumber} = ${firstNumber * secondNumber}` );
 		break;
-	case "div", "/":
-		if ( secondNumber == 0){
-			alert( 'На ноль делить нельзя, пёс. Вводи еще раз!' );
+	case "div":
+		if ( secondNumber === 0){
+			alert( 'На ноль делить нельзя.' );
 			}
 		else
-			alert( firstNumber / secondNumber );
+			alert( `${firstNumber} / ${secondNumber} = ${firstNumber / secondNumber}` );
 		break;
 	default:
 		alert( 'Invalid data. Press F5' );
 }
-/*Задачу выполнил так, как её понял. Может я что-то упустил на занятии и эту задачу 
-можно было выполнить более простыми способами ¯\_(ツ)_/¯*/
