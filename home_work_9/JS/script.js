dd11feb6376793a8ba9de5dd4243df78c44737f2
Georgiy Blagoobrazov <40820747+Nerzrig@users.noreply.github.com>
2021-10-19 16:57:58 +0300

    Add files via upload

--- home_work_9/JS/script.js
+++ home_work_9/JS/script.js
@@ -1,7 +1,44 @@
-const userAge = prompt ( 'Введите свой год рождения.' );
-if( !(userAge === null || userAge.trim() === '' || isNaN( +userAge )) ) {
-	const userCity = prompt ( 'В каком городе Вы живете?' );
-	const userSportsInterest = prompt ( 'Какой ваш любимый вид спорта?' );
+let currentYear = new Date();
+currentYear = currentYear.getFullYear();
+
+let champions = {
+	'настольный теннис': 'Ма Лун',
+	'футбол': 'Рона́лду ди Аси́с Море́йра',
+	'бокс': 'Василий Ломаченко',
+}
+
+let userAge = prompt ( 'Введите год своего рождения.' );
+if( userAge === null ){
+	alert ( 'Жаль, что Вы не захотели ввести свой год рождения.' );
+	userAge = 'надо было ввести год рождения';
 }else{
-	alert( 'Да введи же ты нормальные цифры, дурашка!' );
+	userAge = currentYear - userAge;
+}
+let userCity = prompt ( 'В каком городе Вы живете?' );
+if( userCity === null ){
+	alert ( 'Жаль, что Вы не захотели ввести название города, в котором живете.' );
+	userCity = '- призраке, судя по всему';
+}
+let userSportsInterest = prompt ( 'Какой ваш любимый вид спорта?' );
+if( userSportsInterest === null ){
+	alert ( 'Жаль, что Вы не захотели ввести свой любимый вид спорта.' );
+	userSportsInterest = ' жирдяем.';
+}else if ( champions[userSportsInterest] != undefined ){
+	userSportsInterest = ', как ' + champions[userSportsInterest.toLowerCase()];
+}else{
+	userSportsInterest = ' споротсменом.';
+}
+
+switch(userCity.toLowerCase()){
+	case 'киев':
+		alert( `Тебе ${userAge}. Ты живешь в столице Украины. Круто! Хочешь стать${userSportsInterest}` );
+		break;
+	case 'москва':
+		alert( `Тебе ${userAge}. Ты живешь в столице России. Круто! Хочешь стать${userSportsInterest}` );
+		break;
+	case 'минск':
+		alert( `Тебе ${userAge}. Ты живешь в столице Белоруссии. Круто! Хочешь стать${userSportsInterest}` );
+		break;
+	default:
+		alert( `Тебе ${userAge}. Ты живешь в городе ${userCity}. Круто! Хочешь стать${userSportsInterest}` );
 }
