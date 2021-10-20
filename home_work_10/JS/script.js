652be74f5ecf321d86fb084d9f44b7055560291c
Georgiy Blagoobrazov <40820747+Nerzrig@users.noreply.github.com>
2021-10-20 10:24:07 +0300

    Add files via upload

--- home_work_10/JS/script.js
+++ home_work_10/JS/script.js
@@ -1,67 +1,81 @@
-//Task #1
-
-//После явного преообразования в скобках в Number мы получим явным преобразованием вне скобок Boolean (итого 1, он же true).
-//Но, как мы знаем, арифметические операции "стараются" при их примененни преобразовать типы данных.
-//В итоге Boolean + Number даст нам 2 (а это уже Number, что доказывается выводом данных в консоль).
 /*
-const result = Boolean(Number('10'))+1;
-console.log(result, typeof result);
+//#1
+const x = 10;
+const y = 7;
+x > y ? alert ('x больше, чем y') : alert ('x не больше, чем y');
 */
-
-//В данном примере 'sub' является строкой (т.е. string) состоящей из буквенных символов. Преобразовать в тип отличный от строки мы в данный момент не можем.
-//Поэтому результатом данной операции станет строка "sub0"
 /*
-const result = `sub` + Number(false);
-console.log(result, typeof result);
+//#2
+const num = prompt('Введите число');
+if ( num % 2 === 0){
+	alert ( "Число " + num + " четное" );
+}else{
+	alert ( "Число " + num + " нечетное" );
+}
 */
-
-//В этом примере строкой выступает число обрамленное пробелами. При преобразовании в Number срабатывает что-то типа ф-ции trim(), которая обрезает пробелы.
-//Так как иные символы в строке отсутствуют, то '91' преобразуется в Number благодаря операции умножения
+//#3
 /*
-const result = 16 * '      91    ';
-console.log(result, typeof result);
+let a = prompt ( 'Введите число' );
+let aLen;
+let posOrNeg;
+if ( !(a === null || a.trim() === '' || isNaN( +a )) ){
+	a = a.trim();
+	aLen = a.length;
+	if ( a > 0){
+		posOrNeg = 'положительное';
+	}else{
+		posOrNeg = 'отрицательное';
+		--aLen;
+	}
+	switch (aLen){
+		case 1:
+			alert (`Число ${a} однозначное ${posOrNeg}.`);
+			break;
+		case 2:
+			alert (`Число ${a} двузначное ${posOrNeg}.`);
+			break;
+		case 3:
+			alert (`Число ${a} трехзначное ${posOrNeg}.`);
+			break;	
+		default:
+			alert (`Число ${a} многозначное ${posOrNeg}.`);
+	}
+}else{
+	alert ('Вы ввели не число :(');
+}
 */
-
-//Как и в прошлые разы происходит неявное преобразование типов благодаря математической операции. В итоге 1 - 70 даст результат -69 типа Number
+//#4
 /*
-const result = true - 70;
-console.log(result, typeof result);
+const firstNum = +prompt ( 'Введите первое число ' );
+const secondNum = +prompt ( 'Введите второе число ' );
+const thirdNum = +prompt ( 'Введите третье число' );
+if ( firstNum > secondNum & firstNum > thirdNum){
+	alert ( `Максимальное значение ${firstNum}` );
+}else{
+	if ( secondNum > thirdNum ){
+		alert ( `Максимальное значение ${secondNum}` );
+	}else{
+		alert ( `Максимальное значение ${thirdNum}` );
+	}
+}
 */
-
-//В скобках благодаря явному преобразованию мы получим 11 (строка). Потом преобразуем в Number и 11 сумируем с 1 получая 12 типа Number.
+//#5
 /*
-const result = Number(1 + String(1))+1;
-console.log(result, typeof result);
+const a = +prompt ( 'Введите длину первой стороны треугольника' );
+const b = +prompt ( 'Введите длину второй стороны треугольника ' );
+const c = +prompt ( 'Введите длину третьей стороны треугольника' );
+if( a > 0 & b > 0 & c > 0)
+{
+	if( a + b > c){
+		alert (`Треугольник с длинами сторон ${a}, ${b}, ${c} может сущестовать`);
+	}else if( a + c > b ){
+		alert (`Треугольник с длинами сторон ${a}, ${b}, ${c} может сущестовать`);
+	}else if( b + c > a ){
+		alert (`Треугольник с длинами сторон ${a}, ${b}, ${c} может сущестовать`);
+	}else{
+		alert (`Треугольник с длинами сторон ${a}, ${b}, ${c} не может сущестовать`);
+	}
+}else{
+	alert ('Сторона треугольника не может быть <= 0!');
+}
 */
-
-//Task #2
-
-//Сразу умножил на 3600 секунд (60 сек в минуте * на 60 мин в часе).
-/*
-const secondsInHours = 3600 * +prompt("Enter the number of hours to translate into seconds:");
-alert (`Result: ${secondsInHours} seconds `);
-*/
-
-//Task #3
-
-let num = 1;
-
-//Было
-/*
-num = num + 12;
-num = num — 14;
-num = num * 5;
-num = num / 7;
-num = num + 1;
-num = num — 1;
-*/
-
-//Стало
-num += 12;
-num -= 14;
-num *= 5; 
-num /= 7;
-num++;
-num--;
-
-alert(num);
