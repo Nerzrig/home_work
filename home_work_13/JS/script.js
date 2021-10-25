c0417d8a908dead6d645d3bbfa80cd8aeda46a93
Georgiy Blagoobrazov <40820747+Nerzrig@users.noreply.github.com>
2021-10-25 19:04:30 +0300

    Update script.js

--- home_work_13/JS/script.js
+++ home_work_13/JS/script.js
@@ -1,16 +1,16 @@
 const num = +prompt ( 'Введите число ' );
 let i = 2;
-for (; i <= num; i++) {
-	if (num % i === 0) {
-		break;
-	}
-}
-if ( num < 1){
+if ( num <= 1){
 	console.log ( NaN );
 }else{
+	for (; i <= num; i++) {
+		if (num % i === 0)
+		break;
+	}
 	if ( i === num ){
-		console.log(`Число ${num} простое. А значит, что оно делится на 1 и на ${num}`);
+	console.log(`Число ${num} простое. А значит, что оно делится на 1 и на ${num}`);
 	}else{
-		console.log(`Число ${num} составное, его наименьший делитель ${i}.`);
-		}
+	console.log(`Число ${num} составное, его наименьший делитель ${i}.`);
+	}
 }
+
