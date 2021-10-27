4f8b1b428c494cceef9d3745af8916c4d3d8c1a3
Georgiy Blagoobrazov <gblagoobrazov@gmail.com>
2021-10-27 12:36:14 +0300

    15 hw done!

--- home_work_15/JS/script.js
+++ home_work_15/JS/script.js
@@ -1,20 +1,37 @@
-function subStr(str, strLen, strSym, leftOrRight = true) {
+function padStr(str, strLen, strSym, leftOrRight = true) {
     let message = '';
     if (str === null || str.trim() === '')
         message += 'You interrupted input of the string!\r\n';
-    if (strLen === null)
+    if (strLen === null || strLen.trim() === '')
         message += 'You interrupted the input of the string length!\r\n';
-    if (strSym === null)
+    if (strSym === null || strSym.trim() === '')
         message += 'You interrupted the input of the symbol!\r\n';
-    if (leftOrRight === null)
+    if (leftOrRight === null || leftOrRight.trim() === '')
         message += 'You interrupted the input of the symbol direction!\r\n';
-    if (message !== '')
+    if (message !== '') {
         return message;
-    else
-        return 1;
+    } else {
+        if (strLen > str.length) {
+            strLen -= str.length;
+            if (leftOrRight === true) {
+                for (let i = 0; i < strLen; i++) {
+                    str += strSym;
+                }
+            } else {
+                for (let i = 0; i < strLen; i++) {
+                    strSym += strSym;
+                }
+                str = strSym + str;
+            }
+        } else {
+            console.log(str, str.length, strLen)
+            str = str.substr(0, strLen);
+        }
+        return str;
+    }
 
     //if(str.trim() ==='' || strLen.trim() ==='' || isNaN(+strLen) || strSym === '' || leftOrRight === null)
 }
 
-console.log(subStr(prompt('Enter the string:'), prompt('Enter the string length:'),
+console.log(padStr(prompt('Enter the string:'), prompt('Enter the string length:'),
     prompt('Enter the symbol:'), prompt('Enter the add symbol direction:')));
