32b93176f6dd17de3a816828620a4ad001a0dbeb
Georgiy Blagoobrazov <gblagoobrazov@gmail.com>
2021-10-27 11:36:52 +0300

    14 hw done!

--- home_work_14/JS/script.js
+++ home_work_14/JS/script.js
@@ -1,11 +1,12 @@
-function numPow(num, numP = 1){
-    if(num === null || numP === null){
+function numPow(num, numP = 1) {
+    if (num === null || numP === null) {
         return 'You canceled data entry!';
     }
-    if(num.trim() === '' || isNaN(+num) || numP.trim() === '' || isNaN(+numP)){
+    if (num.trim() === '' || isNaN(+num) || numP.trim() === '' || isNaN(+numP)) {
         return 'Invalid data!';
     }
     console.log(numP);
     return num ** numP;
 }
+
 console.log(numPow(prompt('Enter number:'), prompt('Enter pow:')));
