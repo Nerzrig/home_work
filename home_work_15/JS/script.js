1b8b97f8508f55950f2eb77c39baa2c77dc147b7
Georgiy Blagoobrazov <gblagoobrazov@gmail.com>
2021-10-27 17:06:12 +0300

    15 hw done!

--- home_work_15/JS/script.js
+++ home_work_15/JS/script.js
@@ -1,35 +1,34 @@
 function padStr(str, strLen, strSym, leftOrRight = 'true') {
     let message = '';
-    if (str === null || str.trim() === '')
+    if (str === null || str === '')
         message += 'You interrupted input of the string!\r\n';
-    if (strLen === null || strLen.trim() === '')
+    if (strLen === null || strLen === '')
         message += 'You interrupted the input of the string length!\r\n';
-    if (strSym === null || strSym.trim() === '')
+    if (strSym === null || strSym === '')
         message += 'You interrupted the input of the symbol!\r\n';
-    if (leftOrRight === null || String(leftOrRight.trim()) === '')
+    if (leftOrRight === null || leftOrRight === '')
         message += 'You interrupted the input of the symbol direction!\r\n';
     if (message !== '') {
         return message;
-    } else {
-        if (strLen > str.length) {
-            strLen -= str.length;
-            if (leftOrRight === true) {
-                for (let n = 0; n < strLen; n++) {
-                    str += strSym;
-                }
-            } else {
-                for (let m = 0; m < strLen; m++) {
-                    str = strSym + str;
-                }
+    }
+    if (strLen > str.length) {
+        strLen -= str.length;
+        if (leftOrRight === 'true') {
+            for (let n = 0; n < strLen; n++) {
+                str += strSym;
             }
         } else {
-            str = str.substr(0, strLen);
+            for (let m = 0; m < strLen; m++) {
+                str = strSym + str;
+            }
         }
-        return str;
+    } else {
+        str = str.substr(0, strLen);
     }
+    return str;
 }
 
-/*console.log(padStr(prompt('Enter the string:'), prompt('Enter the string length:'),
-    prompt('Enter the symbol:')));*/
-console.log(padStr(prompt('Enter the string:'), prompt('Enter the string length:'),
-    prompt('Enter the symbol:'), prompt('Enter the add symbol direction:')));
+
+console.log(padStr('hello', 8, '*'));
+console.log(padStr('hello', 6, '*', false));
+console.log(padStr('hello', 2));
