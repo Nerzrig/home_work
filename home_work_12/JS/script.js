a0b3a21b76dbd11f384eae1cff33fc5257983968
George <gblagoobrazov@gmail.com>
2022-09-28 15:55:59 +0300

    1

--- home_work_12/JS/script.js
+++ home_work_12/JS/script.js
@@ -1,14 +1,16 @@
-const num = +prompt ( 'Введите число ' );
-let flag = true;
+(function () {
+    const num = +prompt('Введите число ');
+    let flag = true;
 
-for ( let i = 2; i < num; i++ ) {
-	if ( num % i === 0 ) {
-		flag = false;
-		break;
-	}
-}
-if( flag ){
-	console.log(`Число ${num} простое; // ${flag}. `);
-}else{
-	console.log(`Число ${num} составное // ${flag}.`);
-}
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) {
+            flag = false;
+            break;
+        }
+    }
+    if (flag) {
+        console.log(`Число ${num} простое; // ${flag}. `);
+    } else {
+        console.log(`Число ${num} составное // ${flag}.`);
+    }
+})();
